c55e78e98b505093e26f6549b06c860b9ab28b45
Douglas Cook <douglas.cook@eigentech.com>
2018-05-18 17:43:39 +0100

    Fix async to fetch on mount and swipe

--- app/App.js
+++ app/App.js
@@ -21,46 +21,29 @@ export default class App extends Component {
 
   getDefaultState() {
     return {
-      cards: [{
-        id: '1',
-        applicantName: 'CardView 1',
-        backgroundColor: '#4CAF50'
-      }, {
-        id: '2',
-        applicantName: 'CardView 2',
-        backgroundColor: '#607D8B'
-      }, {
-        id: '3',
-        applicantName: 'CardView 3',
-        backgroundColor: '#9C27B0'
-      }, {
-        id: '4',
-        applicantName: 'CardView 4',
-        backgroundColor: '#00BCD4'
-      }, {
-        id: '5',
-        applicantName: 'CardView 5',
-        backgroundColor: '#FFC107'
-      }],
-      noCards: false,
+      cards: [],
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     if( this.state.cards.length == 0 ) {
-      this.setState(this.getDefaultState());
+      this.getNextApplicant();
     }
   }
 
-  removeCardView(id, judgement) {
-    this.getNextApplicant();
+  passJudgement(id, judgement) {
+    fetch(`${URL}/applicant`, {
+      method: 'PUT',
+      body: JSON.stringify({id: id, decision: judgement})
+    })
+    .then(() => this.removeCard(id))
+    .then(() => this.getNextApplicant())
+  }
 
+  removeCard(id) {
     const newState = {...this.state};
-    newState.cards.splice(this.state.cards.findIndex(x => x.id == id ), 1);
+    newState.cards.splice(this.state.cards.findIndex(x => x.id == id));
     this.setState(newState);
-
-    console.log(judgement);
-    // TODO SEND JUDGEMENT
   }
 
   getNextApplicant() {
@@ -70,6 +53,7 @@ export default class App extends Component {
   }
 
   addApplicantCard(json) {
+    console.log(json);
     const newState = {...this.state};
     newState.cards.push({
       id: json.id,
@@ -86,7 +70,7 @@ export default class App extends Component {
       <Card
         key={key}
         item={item}
-        passJudgement={j => this.removeCardView(item.id, j)}
+        passJudgement={j => this.passJudgement(item.id, j)}
       />
     )
   }
